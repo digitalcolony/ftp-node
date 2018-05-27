4c63095ea70ebb47e09ded69887beeecb972e9e2
MAS <digitalcolony@gmail.com>
2018-05-27 15:37:47 -0700

    list files example

--- listFTPfolder.js
+++ listFTPfolder.js
@@ -1,8 +1,19 @@
-// const c = new Client();
-// c.on("ready", function() {
-//   c.list("/public_html/coffeeclub.app", function(err, list) {
-//     if (err) throw err;
-//     console.dir(list);
-//     c.end();
-//   });
-// });
+const Client = require("ftp");
+require("./config/config");
+
+const folderToList = "/public_html/";
+
+const c = new Client();
+c.on("ready", function() {
+  c.list(folderToList, function(err, list) {
+    if (err) throw err;
+    console.dir(list);
+    c.end();
+  });
+});
+
+c.connect({
+  host: process.env["host"],
+  user: process.env["user"],
+  password: process.env["password"]
+});
